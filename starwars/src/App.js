370c43eef39faa44729b3ddb5cfde2d158dfeb0e
emilie-v11 <emilievukovic79@gmail.com>
2022-05-06 20:39:53 +0200

    Add Header component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,10 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {} from 'react-bootstrap';
+import Header from './components/Header/Header';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="container my-4">
+            <Header title={'Starwars characters'} />
+        </div>
+    );
 }
 
 export default App;
