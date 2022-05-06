90fde45a6c89faa05c2c09414fb1717b8590a29a
emilie-v11 <emilievukovic79@gmail.com>
2022-05-06 21:19:45 +0200

    Add Router

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {} from 'react-bootstrap';
 import Header from './components/Header/Header';
+import Index from './pages/Homepage/Index';
+import Details from './pages/Details/Details';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
     return (
-        <div className="container my-4">
+        <BrowserRouter className="container my-4">
             <Header title={'Starwars characters'} />
-        </div>
+            <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/people/:id" element={<Details />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
