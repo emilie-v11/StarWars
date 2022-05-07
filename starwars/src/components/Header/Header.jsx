d422333bf110d0f814aa028aa222c2b339e9f8b6
emilie-v11 <emilievukovic79@gmail.com>
2022-05-07 20:43:18 +0200

    change color for all elements & add props for color of h1 in Header

--- starwars/src/components/Header/Header.jsx
+++ starwars/src/components/Header/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Header = ({ title, colorTitle }) => {
     return (
         <header className="container mt-4">
-            <h1 style={{color: colorTitle}}>{title}</h1>
+            <h1 className="text-center text-md-start" style={{ color: colorTitle }}>
+                {title}
+            </h1>
         </header>
     );
 };
