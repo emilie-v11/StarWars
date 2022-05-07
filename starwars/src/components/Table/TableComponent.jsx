42fcc316c69e0e0d2f2c5a2a369abcc912919c11
emilie-v11 <emilievukovic79@gmail.com>
2022-05-08 00:05:15 +0200

    Move Table in a Component

---
+++ starwars/src/components/Table/TableComponent.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Table } from 'react-bootstrap';
+import { propTypes } from 'react-bootstrap/esm/Image';
+
+const TableComponent = ({ people }) => {
+    return (
+        <Table className="text-center text-sm-start" striped bordered hover variant="dark">
+            <thead>
+                <tr>
+                    <th className="p-1 py-3 p-sm-3">Name</th>
+                    <th className="p-1 py-3 p-sm-3">Height</th>
+                    <th className="p-1 py-3 p-sm-3">Gender</th>
+                    <th className="p-1 py-3 p-sm-3">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {people.map((character, index) => (
+                    <tr key={index}>
+                        <td className="p-1 py-3 p-sm-3">{character.name}</td>
+                        <td className="p-1 py-3 p-sm-3">{character.height} cm</td>
+                        <td className="p-1 py-3 p-sm-3 text-capitalize">{character.gender}</td>
+                        <td className="p-1 py-3 p-sm-3 position-relative">
+                            <Button
+                                className="position-absolute top-50 start-50 translate-middle py-1 px-lg-4"
+                                variant="warning"
+                            >
+                                View
+                            </Button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
+    );
+};
+
+TableComponent.propTypes = {
+    people: PropTypes.array,
+};
+
+export default TableComponent;
--- starwars/src/pages/Homepage/Index.jsx
+++ starwars/src/pages/Homepage/Index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Button, Table } from 'react-bootstrap';
+// import { Button, Table } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import TableComponent from '../../components/Table/TableComponent';
 
 const Index = () => {
     const isLoading = useSelector(state => state.people.isLoading);
@@ -12,33 +13,7 @@ const Index = () => {
 
     return (
         <main className="Main-Homepage container p-1 pt-5 p-sm-3">
-            <Table className="text-center text-sm-start" striped bordered hover variant="dark">
-                <thead>
-                    <tr>
-                        <th className="p-1 py-3 p-sm-3">Name</th>
-                        <th className="p-1 py-3 p-sm-3">Height</th>
-                        <th className="p-1 py-3 p-sm-3">Gender</th>
-                        <th className="p-1 py-3 p-sm-3">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {people.map((character, index) => (
-                        <tr key={index}>
-                            <td className="p-1 py-3 p-sm-3">{character.name}</td>
-                            <td className="p-1 py-3 p-sm-3">{character.height} cm</td>
-                            <td className="p-1 py-3 p-sm-3 text-capitalize">{character.gender}</td>
-                            <td className="p-1 py-3 p-sm-3 position-relative">
-                                <Button
-                                    className="position-absolute top-50 start-50 translate-middle py-1 px-lg-4"
-                                    variant="warning"
-                                >
-                                    View
-                                </Button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
+            <TableComponent people={people} />
         </main>
     );
 };
