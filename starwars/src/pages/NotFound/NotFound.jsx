1a20b8d135318c6b20e851bdfa44a274a7376bee
emilie-v11 <emilievukovic79@gmail.com>
2022-05-07 17:17:08 +0200

    Add background img website, add design page notfound

--- starwars/src/pages/NotFound/NotFound.jsx
+++ starwars/src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+    return (
+        <main className="Main-Error404 position-relative">
+            <div className="Image-404 position-absolute top-50 start-50 translate-middle">
+                <div className="Content-404 position-absolute top-50 start-50 translate-middle">
+                    <span className="Number-404">404</span>
+                    <p className="Text-404">Oups ! Repartons discrètement ...</p>
 
-export default NotFound
+                    <NavLink className="Back-Home" to="/">
+                        Retourner sur la page d'accueil
+                    </NavLink>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default NotFound;
