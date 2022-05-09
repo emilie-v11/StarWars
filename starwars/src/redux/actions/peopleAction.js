23ebf0ee9ce206dc5222805df95fcbec3b36571c
emilie-v11 <emilievukovic79@gmail.com>
2022-05-09 13:26:26 +0200

    Add actions for get person by ID

--- starwars/src/redux/actions/peopleAction.js
+++ starwars/src/redux/actions/peopleAction.js
@@ -1,8 +1,17 @@
+import { peopleAttributesURL } from '../../services/API-attributesURL';
 import StarwarsService from '../../services/starwars.service';
-import { GET_PEOPLE, GET_PEOPLE_FAIL, GET_PEOPLE_SUCCESS } from './types';
+import {
+    GET_PEOPLE,
+    GET_PEOPLE_FAIL,
+    GET_PEOPLE_SUCCESS,
+    GET_PERSON_BY_ID,
+    GET_PERSON_BY_ID_FAIL,
+    GET_PERSON_BY_ID_SUCCESS,
+} from './types';
 
-export const getPeople = () => dispatch => {
-    return StarwarsService.getAllCharacters().then(
+export const getPeople = option => dispatch => {
+    return StarwarsService.getAPIData(peopleAttributesURL + option).then(
+        // return StarwarsService.getAPIData(peopleAttributesURL, `?page=` + option).then(
         response => {
             dispatch({
                 type: GET_PEOPLE,
@@ -18,9 +27,30 @@ export const getPeople = () => dispatch => {
                 type: GET_PEOPLE_FAIL,
                 payload: error,
             });
-            console.log('Fail to getAPIAllCharacters', error);
+            console.log('Fail to getPeople', error);
         }
     );
 };
 
-// export const get
+export const getPersonById = id => dispatch => {
+    return StarwarsService.getCharacterById(id).then(
+        response => {
+            dispatch({
+                type: GET_PERSON_BY_ID,
+            });
+
+            dispatch({
+                type: GET_PERSON_BY_ID_SUCCESS,
+                payload: response,
+            });
+            console.log(response);
+        },
+        error => {
+            dispatch({
+                type: GET_PERSON_BY_ID_FAIL,
+                payload: error,
+            });
+            console.log('Fail to getPersonById', error);
+        }
+    );
+};
