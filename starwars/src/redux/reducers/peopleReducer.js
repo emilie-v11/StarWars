2e388e8dbc06fba1db01f4487cbfd24d865210fa
emilie-v11 <emilievukovic79@gmail.com>
2022-05-09 13:29:23 +0200

    Add reducer for get person by ID and clean comments

--- starwars/src/redux/reducers/peopleReducer.js
+++ starwars/src/redux/reducers/peopleReducer.js
@@ -1,10 +1,18 @@
-import { GET_PEOPLE, GET_PEOPLE_FAIL, GET_PEOPLE_SUCCESS } from '../actions/types';
+import {
+    GET_PEOPLE,
+    GET_PEOPLE_FAIL,
+    GET_PEOPLE_SUCCESS,
+    GET_PERSON_BY_ID,
+    GET_PERSON_BY_ID_FAIL,
+    GET_PERSON_BY_ID_SUCCESS,
+} from '../actions/types';
 
 const initialState = {
     isLoading: true,
     error: null,
     currentPage: 1,
     characters: [],
+    person: {},
 };
 
 const people = (state = initialState, action) => {
@@ -22,14 +30,37 @@ const people = (state = initialState, action) => {
             return {
                 ...state,
                 isLoading: false,
+                error: false,
+                id: payload.results.url ? payload.results.url.slice(29) : payload.results.url,
+                count: payload.count,
                 characters: payload.results,
                 currentPage:
-                    payload.previous !== null ? Number(payload.previous.substr(-1)) + 1 : Number(1),
-                // : Number(payload.next.substr(-1)) - 1,
-                error: false,
+                    payload.previous !== null ? Number(payload.previous.slice(-1)) + 1 : Number(1),
             };
 
         case GET_PEOPLE_FAIL:
+            return {
+                ...state,
+                isLoading: false,
+                error: true,
+            };
+
+        case GET_PERSON_BY_ID:
+            return {
+                ...state,
+                isLoading: true,
+                error: false,
+            };
+
+        case GET_PERSON_BY_ID_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                error: false,
+                person: payload,
+            };
+
+        case GET_PERSON_BY_ID_FAIL:
             return {
                 ...state,
                 isLoading: false,
