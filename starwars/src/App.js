b8dd6bab2b8f5485ed8e53c69753d7402d628f38
emilie-v11 <emilievukovic79@gmail.com>
2022-05-09 22:52:48 +0200

    Add Lazy and Suspense for perf

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,14 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import {} from 'react-bootstrap';
 import Header from './components/Header/Header';
-import Index from './pages/Homepage/Index';
-import Details from './pages/Details/Details';
-import NotFound from './pages/NotFound/NotFound';
 import LoaderSpinner from './components/LoaderSpinner/LoaderSpinner';
 import { getPeople } from './redux/actions/peopleAction';
+
+const Index = lazy(() => import('./pages/Homepage/Index'));
+const Details = lazy(() => import('./pages/Details/Details'));
+const NotFound = lazy(() => import('./pages/NotFound/NotFound'));
+const renderLoader = () => <LoaderSpinner />;
 
 function App() {
     const dispatch = useDispatch();
@@ -25,11 +27,13 @@ function App() {
     return (
         <BrowserRouter className="container my-4">
             <Header title="Starwars characters" colorTitle="#FFC106" />
-            <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="/people/:id" element={<Details />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
+            <Suspense fallback={renderLoader()}>
+                <Routes>
+                    <Route path="/" element={<Index />} />
+                    <Route path="/people/:id" element={<Details />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     );
 }
