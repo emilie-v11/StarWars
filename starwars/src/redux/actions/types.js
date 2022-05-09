40a239af1f5290c7833ce0a3dcf6fc3141cea0f3
emilie-v11 <emilievukovic79@gmail.com>
2022-05-09 12:47:43 +0200

    Add news types for request person by ID

--- starwars/src/redux/actions/types.js
+++ starwars/src/redux/actions/types.js
@@ -1,4 +1,9 @@
-// CHARACTER
+// All the People data
 export const GET_PEOPLE = 'GET_PEOPLE';
 export const GET_PEOPLE_SUCCESS = 'GET_PEOPLE_SUCCESS';
 export const GET_PEOPLE_FAIL = 'GET_PEOPLE_FAIL';
+
+// person BY ID data
+export const GET_PERSON_BY_ID = 'GET_PERSON_BY_ID';
+export const GET_PERSON_BY_ID_SUCCESS = 'GET_PERSON_BY_ID_SUCCESS';
+export const GET_PERSON_BY_ID_FAIL = 'GET_PERSON_BY_ID_FAIL';
