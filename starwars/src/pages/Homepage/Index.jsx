e64aae1144687d69e035ba126137c9b2309fd1a5
emilie-v11 <emilievukovic79@gmail.com>
2022-05-09 13:51:39 +0200

    Fixe and clean Index homepage page

--- starwars/src/pages/Homepage/Index.jsx
+++ starwars/src/pages/Homepage/Index.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
-// import { Button, Table } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import PaginationRounded from '../../components/Pagination/Pagination';
 import TableComponent from '../../components/Table/TableComponent';
+import { getPeople } from '../../redux/actions/peopleAction';
 
 const Index = () => {
-    const isLoading = useSelector(state => state.people.isLoading);
-    const currentPage = useSelector(state => state.people.currentPage);
+    const dispatch = useDispatch();
+
     const people = useSelector(state => state.people.characters);
-    console.log(isLoading);
-    console.log(currentPage);
-    console.log(people);
+    const currentPage = useSelector(state => state.people.currentPage);
+    const count = useSelector(state => state.people.count);
+    
+    const [page, setPage] = useState(currentPage);
+
+    useEffect(() => {
+        dispatch(getPeople(`?page=${page}`));
+    }, [dispatch, page]);
+
+    const handleChange = (event, value) => {
+        setPage(value);
+    };
+
+    const totalPages = Math.ceil(count / 10);
+    console.log(totalPages);
 
     return (
-        <main className="Main-Homepage container p-1 pt-5 p-sm-3">
+        <main className="Main-Homepage container p-1 pt-4 p-sm-3 mt-5">
             <TableComponent people={people} />
+
+            <div className="d-flex justify-content-end text-light pt-2">
+                <PaginationRounded
+                    page={page}
+                    handleChange={handleChange}
+                    totalPages={totalPages}
+                />
+            </div>
         </main>
     );
 };
