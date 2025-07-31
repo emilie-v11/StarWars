f50171fc66783aa4f3506cb44497c668b3f59711
Vukovic Emilie <emilievukovic79@gmail.com>
2025-07-31 17:55:18 +0200

    feat: change baseURL for swapi.info/api instead of swapi.tech/api

--- src/services/API-baseURL.js
+++ src/services/API-baseURL.js
@@ -3,5 +3,5 @@
  * @property {string} baseURL - URL to the API
  */
 // export const baseURL = 'https://swapi.dev/api/';
-// export const baseURL = 'https://swapi.info/api/';
-export const baseURL = 'https://www.swapi.tech/api/';
+export const baseURL = 'https://swapi.info/api/';
+// export const baseURL = 'https://www.swapi.tech/api/';
