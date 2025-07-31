a0fd0bc4d96334cf30ff7c21fbd300152269d343
Vukovic Emilie <emilievukovic79@gmail.com>
2025-07-31 17:56:46 +0200

    feat: cleaning up previous API baseURL

--- src/services/API-baseURL.js
+++ src/services/API-baseURL.js
@@ -2,6 +2,5 @@
  * URL Base for API Starwars.
  * @property {string} baseURL - URL to the API
  */
-// export const baseURL = 'https://swapi.dev/api/';
+
 export const baseURL = 'https://swapi.info/api/';
-// export const baseURL = 'https://www.swapi.tech/api/';
