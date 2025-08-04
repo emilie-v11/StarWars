31728ad5d01bff836b0664865ec3f6cda64d89f0
Vukovic Emilie <emilievukovic79@gmail.com>
2025-08-04 14:26:25 +0200

    feat: cleaning up previous files Redux

--- src/redux/actions/types.js
+++
@@ -1,9 +0,0 @@
-// Types of actions for Redux for all the People data
-export const GET_PEOPLE = 'GET_PEOPLE';
-export const GET_PEOPLE_SUCCESS = 'GET_PEOPLE_SUCCESS';
-export const GET_PEOPLE_FAIL = 'GET_PEOPLE_FAIL';
-
-// Types of actions for Redux for a person by ID data
-export const GET_PERSON_BY_ID = 'GET_PERSON_BY_ID';
-export const GET_PERSON_BY_ID_SUCCESS = 'GET_PERSON_BY_ID_SUCCESS';
-export const GET_PERSON_BY_ID_FAIL = 'GET_PERSON_BY_ID_FAIL';
