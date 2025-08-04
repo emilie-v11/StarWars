67371381ad997972fe3df325869db3e8216d85d5
Vukovic Emilie <emilievukovic79@gmail.com>
2025-08-04 14:59:20 +0200

    fix: unmounted currentPerson to fix currentPerson name in breadscrumbs persistance in Homepage

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,30 +1,39 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { useGetPersonByIdQuery } from '@/services/peopleApi';
+import { clearCurrentPerson } from '@/store/slices/peopleSlice';
 import InformationSheet from '@/components/InformationSheet/InformationSheet';
 import LoaderSpinner from '@/components/LoaderSpinner/LoaderSpinner';
-import { useGetPersonByIdQuery } from '../../services/peopleApi';
-// import { useSelector } from 'react-redux';
 
 /**
  * Details Page - For more informations of the active personn (by ID) - Access to this page with button "view" in the Table
  */
 const Details = () => {
-    const { id } = useParams();
-    const { data: person, isLoading, error } = useGetPersonByIdQuery(Number(id), {
-        skip: !id,
-    });
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const { data: person, isLoading, error } = useGetPersonByIdQuery(Number(id), {
+    skip: !id,
+  });
 
-    if (isLoading) return <LoaderSpinner />;
-    if (error) return <div>Error: { error.message }</div>;
+  useEffect(() => {
+    return () => {
+      dispatch(clearCurrentPerson());
+    };
+  }, [dispatch]);
 
-    return (
-        <main className="Main-Details container mt-5 position-relative text-white py-2">
-            <section>
-                <h2 className="visually-hidden"> Information sheet of {person.name}</h2>
+  if (isLoading) return <LoaderSpinner />;
+  if (error) return <div>Error: { error.message }</div>;
 
-                <InformationSheet person={person} />
-            </section>
-        </main>
-    );
+  return (
+    <main className="Main-Details container mt-5 position-relative text-white py-2">
+      <section>
+        <h2 className="visually-hidden"> Information sheet of { person.name }</h2>
+
+        <InformationSheet person={ person } />
+      </section>
+    </main>
+  );
 };
 
 export default Details;
